4f40034ef3bdf118856f2d86a85fcbe071cf7930
Maya Rashish <maya.rashish@runtimeverification.com>
2018-09-07 20:13:19 +0300

    Give ourselves a 10min timeout, don't try adjusting travis_wait

--- rv.sh
+++ rv.sh
@@ -8,7 +8,7 @@ make
 
 export RVP_ANALYSIS_ARGS="--output=json"
 export RVP_REPORT_FILE="$PWD/my_errors.json"
-timeout 20m make check V=1
+timeout 10m make check V=1
 
 report_path="$PWD/report"
 touch $RVP_REPORT_FILE && rv-html-report $RVP_REPORT_FILE -o $report_path
